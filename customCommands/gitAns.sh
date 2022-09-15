4a05646f514c042ea11d66dc610d50fc2740c36b
ldraney <draneylucas@gmail.com>
2022-09-15 15:51:13 -0600

    first commit

---
+++ customCommands/gitAns.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+cd $HOME/github/dotfiles
+git add -A && git commit -m $1 && git push
+cd $HOME/github/ansible_desktop_setup
+git add -A && git commit -m $1 && git push
+cd $HOME/github/sensitive
+git add -A && git commit -m $1 && git push
+
+sudo ansible-pull -U git@github.com:ldraney/ansible_desktop_setup.git --key-file /home/ldraney/.ssh/id_ed25519
