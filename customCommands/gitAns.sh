18691d6c082e1e2b1561b3663a2c5d45335362b3
ldraney <draneylucas@gmail.com>
2022-09-21 15:06:44 -0600

    git

--- customCommands/gitAns.sh
+++ customCommands/gitAns.sh
@@ -2,11 +2,11 @@
 
 cd $HOME/github/dotfiles
 #have a git stash store so that if there's an conflict I can resolve with the stash and not lose my work
-git stash store "$(git stash create)"
-git add -A && git commit -m $1 && git push
+#git stash store "$(git stash create)"
+git pull && git add -A && git commit -m $1 && git push
 cd $HOME/github/ansible_desktop_setup
-git add -A && git commit -m $1 && git push
+git pull && git add -A && git commit -m $1 && git push
 cd $HOME/github/sensitive
-git add -A && git commit -m $1 && git push
+git pull && git add -A && git commit -m $1 && git push
 
 sudo ansible-pull -U git@github.com:ldraney/ansible_desktop_setup.git --key-file /home/ldraney/.ssh/id_ed25519 
--- customCommands/gitScript.sh
+++ customCommands/gitScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-git stash store "$(git stash create)"
-git add -A && git commit -m $1 && git push
+#git stash store "$(git stash create)"
+git pull && git add -A && git commit -m $1 && git push
+#git add -A && git commit -m $1 && git push
