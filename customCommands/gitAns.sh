e90aa122086e97580c4b75ae8b78af0654a4b063
ldraney <draneylucas@gmail.com>
2022-09-21 15:22:07 -0600

    just testing

--- customCommands/gitAns.sh
+++ customCommands/gitAns.sh
@@ -2,17 +2,22 @@
 #set up a stash system so all conflicts are handled with stashes rather than the 
 #main repo, thus taking the risk of accidentally overwriting the current branch 
 #or losing local changes
-git config pull.rebase true
-git config rebase.autoStash true
+#git config pull.rebase true
+#git config rebase.autoStash true
+#or perhaps
+##git stash store "$(git stash create)"
 
 cd $HOME/github/dotfiles
-#have a git stash store so that if there's an conflict I can resolve with the 
-#stash and not lose my work
-#git stash store "$(git stash create)"
+git config pull.rebase true
+git config rebase.autoStash true
 git pull && git add -A && git commit -m $1 && git push
 cd $HOME/github/ansible_desktop_setup
+git config pull.rebase true
+git config rebase.autoStash true
 git pull && git add -A && git commit -m $1 && git push
 cd $HOME/github/sensitive
+git config pull.rebase true
+git config rebase.autoStash true
 git pull && git add -A && git commit -m $1 && git push
 
 sudo ansible-pull -U git@github.com:ldraney/ansible_desktop_setup.git --key-file /home/ldraney/.ssh/id_ed25519 
