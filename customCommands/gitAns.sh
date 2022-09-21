0511c21f12205c52d26dfcb50a57cd60d025c16c
ldraney <draneylucas@gmail.com>
2022-09-21 14:39:26 -0600

    adjusted git script tools

--- customCommands/gitAns.sh
+++ customCommands/gitAns.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/zsh
 
 cd $HOME/github/dotfiles
+#have a git stash store so that if there's an conflict I can resolve with the stash and not lose my work
+git stash store "$(git stash create)"
 git add -A && git commit -m $1 && git push
 cd $HOME/github/ansible_desktop_setup
 git add -A && git commit -m $1 && git push
--- customCommands/gitScript.sh
+++ customCommands/gitScript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+git stash store "$(git stash create)"
 git add -A && git commit -m $1 && git push
