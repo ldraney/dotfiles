faa5f23c1228edcef9d8ad51968cdb94690b3f9d
ldraney <draneylucas@gmail.com>
2022-09-21 15:20:44 -0600

    just testing

--- customCommands/gitAns.sh
+++ customCommands/gitAns.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/zsh
+#set up a stash system so all conflicts are handled with stashes rather than the 
+#main repo, thus taking the risk of accidentally overwriting the current branch 
+#or losing local changes
+git config pull.rebase true
+git config rebase.autoStash true
 
 cd $HOME/github/dotfiles
-#have a git stash store so that if there's an conflict I can resolve with the stash and not lose my work
+#have a git stash store so that if there's an conflict I can resolve with the 
+#stash and not lose my work
 #git stash store "$(git stash create)"
 git pull && git add -A && git commit -m $1 && git push
 cd $HOME/github/ansible_desktop_setup
